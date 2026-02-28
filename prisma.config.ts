1619215b16158454fb249f147a5fd050cf526235
JesusLeonelCastro <jesusleonelcastro11@gmail.com>
2026-02-28 17:45:50 -0500

    Error en Confiurar prisma archivo seed

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,9 +5,8 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-    seed: 'npx ts-node prisma/seed.ts',
+    seed: "tsx prisma/seed.ts",
   },
-  
   datasource: {
     url: env("DATABASE_URL"),
   },
